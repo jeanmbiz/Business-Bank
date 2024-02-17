2ce90423e3b00146e4d9207343cdca98358e3e70
jeanmbiz <jeanmbiz@hotmail.com>
2024-02-17 17:19:17 -0300

    feat: add rules on create deposit

--- app/Http/Requests/CreateDepositRequest.php
+++ app/Http/Requests/CreateDepositRequest.php
@@ -11,7 +11,7 @@ class CreateDepositRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return auth()->user()->id == $this->route('id');
+        return true;
     }
 
     /**
@@ -22,16 +22,23 @@ class CreateDepositRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'receiverCpf' => ['required', 'string', 'regex:/^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/'],
             'value' => ['required', 'numeric', 'gte:0.01'],
+            'payerCpf' => ['string', 'regex:/^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/'],
         ];
     }
 
     public function messages(): array
     {
         return [
+            'receiverCpf.required' => 'CPF é obrigatóro',
+            'receiverCpf.string' => 'CPF deve ser do tipo string',
+            'receiverCpf.regex' => 'CPF deve ser válido',
             'value.required' => 'O valor é obrigatório',
             'value.numeric' => 'O valor deve ser um número',
             'value.gte' => 'O valor deve ser maior ou igual a 0.01',
+            'payerCpf.string' => 'CPF deve ser do tipo string',
+            'payerCpf.regex' => 'CPF deve ser válido',
         ];
     }
 }
