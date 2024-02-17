8a46c52db90d479de1e68925af4676acbde99001
jeanmbiz <jeanmbiz@hotmail.com>
2024-02-17 17:18:06 -0300

    refactor: add injection dependence on CreateDepositService

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,15 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateDepositRequest;
 use App\Http\Requests\CreateTransactionRequest;
+use App\Services\CreateDepositService;
 use App\Services\CreateTransactionService;
 
 class TransactionController extends Controller
 {
+    protected $createDepositService;
+
+    public function __construct(CreateDepositService $createDepositService)
+    {
+        $this->createDepositService = $createDepositService;
+    }
+
+    public function deposit(CreateDepositRequest $request)
+    {
+        return $this->createDepositService->execute($request->all());
+    }
+
+
     public function create(CreateTransactionRequest $request)
     {
         $createTransactionService = new CreateTransactionService();
 
         return $createTransactionService->execute($request->all());
     }
+
+
 }
