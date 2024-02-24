0b8a3d0495af9d40fce3e01674755e00dd62cd28
jeanmbiz <jeanmbiz@hotmail.com>
2024-02-24 17:37:30 -0300

    refactor: user delete logic

--- app/Services/User/DeleteUserService.php
+++ app/Services/User/DeleteUserService.php
@@ -2,23 +2,13 @@
 
 namespace App\Services\User;
 
-use App\Exceptions\AppError;
-use App\Models\User;
 use Illuminate\Http\Request;
 
 class DeleteUserService
 {
     public function delete(Request $request)
     {
-        $userId = $request->route('userId');
-
-        $user = User::find($userId);
-
-        if (is_null($user)) {
-            throw new AppError('Usuário não existe', 404);
-        }
-
-        $user->delete();
+        $request['user_DB']->delete();
 
         return response()->noContent();
     }
