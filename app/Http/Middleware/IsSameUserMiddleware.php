438acc880556ef183fdd4309e62777b8b62c6406
jeanmbiz <jeanmbiz@hotmail.com>
2024-02-24 17:33:19 -0300

    refact: add user information and checks in request

--- app/Http/Middleware/IsSameUserMiddleware.php
+++ app/Http/Middleware/IsSameUserMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Exceptions\AppError;
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,11 +12,21 @@ class IsSameUserMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        $user = Auth::user();
+        $userId = $request->route('userId');
 
-        if ($user->id !== $request->route('userId')) {
+        $paramUser = User::find($userId);
+
+        if (is_null($paramUser)) {
+            throw new AppError('Usuário não existe', 404);
+        }
+
+        if ($request['user_id'] !== $userId) {
             throw new AppError('Este recurso está disponivel apenas para o próprio usuário', 403);
         }
+
+        $request->merge([
+            'user_DB' => $paramUser
+        ]);
 
         return $next($request);
     }
